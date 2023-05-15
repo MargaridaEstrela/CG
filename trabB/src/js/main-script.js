ef871fd2b3141f1329211a210ef350ff129c7cd4
Bernardo Jordão <bernardo.jordao@tecnico.ulisboa.pt>
2023-05-15 16:59:12 +0100

    add cameras

--- trabB/src/js/main-script.js
+++ trabB/src/js/main-script.js
@@ -1,8 +1,10 @@
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
-var camera, scene, renderer;
+// Cameras
+var cameraFront, cameraSide, cameraTop, cameraOrtographic, cameraPerspective;
 
+var camera, renderer, scene;
 var geometry, material, mesh;
 
 /////////////////////
@@ -21,18 +23,93 @@ function createScene(){
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
-function createCamera() {
-    'use strict';
-    camera = new THREE.PerspectiveCamera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-    camera.lookAt(scene.position);
+function createCameraFront() {
+
+    cameraFront = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+    
+    cameraFront.position.x = 0;
+    cameraFront.position.y = 4;
+    cameraFront.position.z = 40;
+    cameraFront.lookAt(0, 4, 0);
 }
 
+function createCameraSide() {
+
+    cameraSide = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    );
+
+    cameraSide.position.x = 40;
+    cameraSide.position.y = 4;
+    cameraSide.position.z = 0;
+    cameraSide.lookAt(0, 4, 0);
+
+}
+
+function createCameraTop() {
+
+    cameraTop = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+
+    cameraTop.position.x = 0;
+    cameraTop.position.y = 40;
+    cameraTop.position.z = 0;
+    cameraTop.lookAt(0, 0, 0);
+}
+
+function createCameraOrthographic() {
+    
+    cameraOrtographic = new THREE.OrthographicCamera(
+        window.innerWidth / -20,
+        window.innerWidth / 20,
+        window.innerHeight / 20,
+        window.innerHeight / -20,
+        -1000,
+        1000,
+    )
+
+    cameraOrtographic.position.x = 30;
+    cameraOrtographic.position.y = 30;
+    cameraOrtographic.position.z = 30;
+    cameraOrtographic.lookAt(0, 0, 0);
+
+}
+
+function createCameraPerspective() {
+    
+    cameraPerspective = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+
+    cameraPerspective.position.x = 30;
+    cameraPerspective.position.y = 30;
+    cameraPerspective.position.z = 30;
+    cameraPerspective.lookAt(0, 0, 0);
+
+}
+
+function createCameras() {
+    createCameraFront();
+    createCameraSide();
+    createCameraTop();
+    createCameraOrthographic();
+    createCameraPerspective();
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -139,7 +216,9 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     createScene();
-    createCamera();
+    createCameras();
+
+    camera = cameraFront;
 
     render();
 
@@ -178,6 +257,23 @@ function onResize() {
 function onKeyDown(e) {
     'use strict';
 
+    switch (e.keyCode) {
+        case 49: //1
+            camera = cameraFront;
+            break;
+        case 50: //2
+            camera = cameraSide;
+            break;
+        case 51: //3
+            camera = cameraTop;
+            break;
+        case 52: //4
+            camera = cameraOrtographic;
+            break;
+        case 53: //5
+            camera = cameraPerspective;
+            break;
+    }
 }
 
 ///////////////////////
