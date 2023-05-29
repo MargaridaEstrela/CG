8831a510b32e978f1379c47a725d510c597be1f6
= <bernardo.jordao@tecnico.ulisboa.pt>
2023-05-29 23:20:36 +0100

    basic template to render

---
+++ trabC/src/js/cameras.js
@@ -0,0 +1,79 @@
+
+function createCameraFront() {
+
+    cameras.front = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+
+    cameras.front.position.x = 0;
+    cameras.front.position.y = 30;
+    cameras.front.position.z = 100;
+    cameras.front.lookAt(0, 0, 0);
+}
+
+function createCameraSide() {
+
+    cameras.side = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+
+    cameras.side.position.x = 110;
+    cameras.side.position.y = 10;
+    cameras.side.position.z = 0;
+    cameras.side.lookAt(0, 0, 0);
+}
+
+function createCameraTop() {
+
+    cameras.top = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+
+    cameras.top.position.x = 0;
+    cameras.top.position.y = 150;
+    cameras.top.position.z = 0;
+    cameras.top.lookAt(0, 0, 0);
+}
+
+function createCameraOrthographic() {
+    
+    cameras.ortographic = new THREE.OrthographicCamera(
+        window.innerWidth / -12,
+        window.innerWidth / 12,
+        window.innerHeight / 12,
+        window.innerHeight / -12,
+        -1000,
+        1000,
+    )
+
+    cameras.ortographic.position.x = 30;
+    cameras.ortographic.position.y = 30;
+    cameras.ortographic.position.z = 30;
+    cameras.ortographic.lookAt(0, 0, 0);
+
+}
+
+function createCameraPerspective() {
+    
+    cameras.perspective = new THREE.PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+
+    cameras.perspective.position.x = 50;
+    cameras.perspective.position.y = 50;
+    cameras.perspective.position.z = 50;
+    cameras.perspective.lookAt(0, 0, 0);
+
+}
--- trabC/src/js/main-script.js
+++ trabC/src/js/main-script.js
@@ -1,7 +1,13 @@
 //////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
+//General
+var camera, renderer, scene, axis;
+var geometry, mesh;
 
+var materials = {};
+var cameras = {};
+var keysPressed = {};
 
 /////////////////////
 /* CREATE SCENE(S) */
@@ -9,12 +15,24 @@
 function createScene(){
     'use strict';
 
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0.9,0.9,0.9);
+    axis = new THREE.AxesHelper(10);
+    axis.visible = false;
+    scene.add(axis);
+
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
-
+function createCameras() {
+    createCameraFront();
+    createCameraSide();
+    createCameraTop();
+    createCameraOrthographic();
+    createCameraPerspective();
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -53,7 +71,7 @@ function update(){
 /////////////
 function render() {
     'use strict';
-
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////////
@@ -61,7 +79,22 @@ function render() {
 ////////////////////////////////
 function init() {
     'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
+    createScene();
+    createCameras();
+    
+    camera = cameras.front;
+
+    render();
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
+    window.addEventListener("resize", onResize);
 }
 
 /////////////////////
@@ -70,6 +103,11 @@ function init() {
 function animate() {
     'use strict';
 
+    update();
+
+    render();
+
+    requestAnimationFrame(animate);
 }
 
 ////////////////////////////
@@ -77,6 +115,12 @@ function animate() {
 ////////////////////////////
 function onResize() { 
     'use strict';
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    }
 
 }
 
