1719e45198182c677e60a7b54d9cf6a83fdccc54
MargaridaEstrela <margarida.e2002@gmail.com>
2023-06-05 16:18:16 +0100

    fix: house with triangles

--- trabC/src/js/house.js
+++ trabC/src/js/house.js
@@ -5,29 +5,72 @@ function addHouseStructure(obj, x, y, z){
     geometry = new THREE.BufferGeometry();
 
     var vertices = [
-        -5.4, 0, 2.2,     // Vertex 0: front left bottom
-        -5.4, 3.3, 2.2,   // Vertex 1: front left top
-        5.4, 0, 2.2,      // Vertex 2: front right bottom
-        5.4, 3.3, 2.2,    // Vertex 3: front right top
-        -5.4, 0, -2.2,    // Vertex 4: back left bottom
-        -5.4, 3.3, -2.2,  // Vertex 5: back left top
-        5.4, 0, -2.2,     // Vertex 6: back right bottom
-        5.4, 3.3, -2.2,   // Vertex 7: back right top
+        -5.125, 0.2, 2.2,     // Vertex 0: front left bottom
+        -5.125, 3.3, 2.2,     // Vertex 1: front left top
+        5.125, 0.2, 2.2,      // Vertex 2: front right bottom
+        5.125, 3.3, 2.2,      // Vertex 3: front right top
+        -5.125, 0.2, -2.2,    // Vertex 4: back left bottom
+        -5.125, 3.3, -2.2,    // Vertex 5: back left top
+        5.125, 0.2, -2.2,     // Vertex 6: back right bottom
+        5.125, 3.3, -2.2,     // Vertex 7: back right top
+
+        -4.125, 0.2, 2.2,     // Vertex 8
+        -3.575, 0.2, 2.2,     // Vertex 9
+        -2.475, 0.2, 2.2,     // Vertex 10
+        -1.375, 0.2, 2.2,     // Vertex 11
+        -0.275, 0.2, 2.2,     // Vertex 12
+
+        -4.125, 1.65, 2.2,    // Vertex 13
+        -3.575, 1.65, 2.2,    // Vertex 14
+
+        -5.125, 2.2, 2.2,     // Vertex 15
+        -4.125, 2.2, 2.2,     // Vertex 16
+        -3.575, 2.2, 2.2,     // Vertex 17
+        -2.475, 2.2, 2.2,     // Vertex 18
+        -1.375, 2.2, 2.2,     // Vertex 19
+
+        -1.375, 3.3, 2.2,     // Vertex 20
+        -0.275, 3.3, 2.2,     // Vertex 21
+
+        0.275, 0.2, 2.2,      // Vertex 22
+        0.275, 1.65, 2.2,     // Vertex 23
+        1.375, 1.65, 2.2,     // Vertex 24
+        1.925, 1.65, 2.2,     // Vertex 25
+        3.575, 1.65, 2.2,     // Vertex 26
+        4.125, 1.65, 2.2,     // Vertex 27
+        5.675, 1.65, 2.2,     // Vertex 28
+
+        0.275, 2.2, 2.2,      // Vertex 29
+        1.375, 2.2, 2.2,      // Vertex 30
+        1.925, 2.2, 2.2,      // Vertex 31
+        3.575, 2.2, 2.2,      // Vertex 32
+        4.125, 2.2, 2.2,      // Vertex 33
+        5.675, 2.2, 2.2,      // Vertex 34
+
+        0.275, 3.3, 2.2       // Vertex 35
     ];
 
     var indices = [
-        1, 0, 2,  // Triangle 1
-        1, 2, 3,  // Triangle 2
-        1, 3, 5,  // Triangle 3
-        5, 3, 7,  // Triangle 4
-        5, 4, 6,  // Triangle 5
-        5, 6, 7,  // Triangle 6
-        0, 4, 5,  // Triangle 7
-        1, 0, 5,  // Triangle 8
-        6, 2, 7,  // Triangle 9
-        2, 3, 7,  // Triangle 10
-        0, 2, 4,  // Triangle 11
-        4, 2, 6  // Triangle 12
+        0, 8, 15,
+        15, 8, 16,
+        15, 19, 1,
+        1, 19, 20,
+        11, 12, 20,
+        20, 12, 21,
+        9, 10, 17,
+        17, 10, 18,
+        8, 9, 13,
+        13, 9, 14,
+        22, 2, 23,
+        23, 2, 28,
+        23, 24, 29,
+        29, 24, 30,
+        25, 26, 31,
+        31, 26, 32,
+        27, 28, 33,
+        33, 28, 34,
+        29, 34, 35,
+        35, 34, 3
     ];
 
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
@@ -62,8 +105,8 @@ function addDoor(obj, x, y, z){
     ];
 
     var indices = [
-        0, 1, 2,  // Triangle 1
-        2, 1, 3,  // Triangle 2
+        0, 1, 2,
+        2, 1, 3
     ];
 
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
@@ -74,7 +117,7 @@ function addDoor(obj, x, y, z){
     geometry.computeVertexNormals(); 
 
     var doorMaterial = new THREE.MeshBasicMaterial();
-    doorMaterial.color.set("brown");
+    doorMaterial.color.set("royalblue");
 
     var doorMesh = new THREE.Mesh(geometry, doorMaterial);
     doorMesh.position.set(x, y, z);
@@ -104,20 +147,19 @@ function addWindows(obj, x, y, z){
         1.925, 2.2, 2.2,        // Vertex 7: top right
 
         // Front right window
-        3.025, 1.65, 2.2,     // Vertex 8: bottom left
-        3.575, 1.65, 2.2,     // Vertex 9: bottom right
-        3.025, 2.2, 2.2,   // Vertex 10: top left
-        3.575, 2.2, 2.2    // Vertex 11: top right
+        3.575, 1.65, 2.2,     // Vertex 8: bottom left
+        4.125, 1.65, 2.2,     // Vertex 9: bottom right
+        3.575, 2.2, 2.2,      // Vertex 10: top left
+        4.125, 2.2, 2.2,    // Vertex 11: top right
     ];
 
     var indices = [
-        0, 1, 2,    // Triangle 1
-        2, 1, 3,    // Triangle 2
-        4, 5, 6,    // Triangle 3
-        6, 5, 7,    // Triangle 4
-        8, 9, 10,   // Triangle 5
-        10, 9, 11   // Triangle 6
-
+        0, 1, 2,
+        2, 1, 3,
+        4, 5, 6,
+        6, 5, 7,
+        8, 9, 10,
+        10, 9, 11
     ];
 
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
@@ -128,7 +170,7 @@ function addWindows(obj, x, y, z){
     geometry.computeVertexNormals(); 
 
     var doorMaterial = new THREE.MeshBasicMaterial();
-    doorMaterial.color.set("deepskyblue");
+    doorMaterial.color.set("royalblue");
 
     var doorMesh = new THREE.Mesh(geometry, doorMaterial);
     doorMesh.position.set(x, y, z);
@@ -146,21 +188,21 @@ function addRoof(obj, x, y, z) {
 
     var vertices = [
         // Left triangle
-        -5.4, 3.3, 2.2,    // Vertex 0
-        -5.4, 3.3, -2.2,   // Vertex 1
-        -5.4, 3.85, 0,     // Vertex 3
+        -5.675, 3.3, 2.2,    // Vertex 0
+        -5.675, 3.3, -2.2,   // Vertex 1
+        -5.675, 3.85, 0,     // Vertex 3
 
         // Right triangle
-        5.4, 3.3, 2.2,     // Vertex 4
-        5.4, 3.3, -2.2,    // Vertex 5
-        5.4, 3.85, 0,      // Vertex 6  
+        5.675, 3.3, 2.2,     // Vertex 4
+        5.675, 3.3, -2.2,    // Vertex 5
+        5.675, 3.85, 0,      // Vertex 6  
     ];
 
     var indices = [
-        0, 1, 2,    // Triangle 1
-        3, 4, 5,    // Triangle 2
-        0, 3, 2,    // Triangle 3
-        2, 3, 5,    // Triangle 4
+        0, 1, 2,    
+        3, 4, 5,    
+        0, 3, 2,    
+        2, 3, 5, 
     ];
 
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
@@ -199,8 +241,8 @@ function addChimney(obj, x, y, z){
     ];
 
     var indices = [
-        0, 1, 2,  // Triangle 1
-        2, 1, 3,  // Triangle 2
+        0, 1, 2,  
+        2, 1, 3,  
     ];
 
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
@@ -228,12 +270,12 @@ function addCollumns(obj, x, y, z){
     geometry = new THREE.BufferGeometry();
 
     var vertices = [
-        -5.4, 0, 2.2,     // Vertex 0
-        -5.4, 0, 2.75,    // Vertex 1
-        -4.85, 0, 2.2,      // Vertex 2
-        -4.85, 0, 2.75,     // Vertex 3
-        -4.85, 3.3, 2.2,    // Vertex 4
-        -5.4, 3.3, 2.2,   // Vertex 5
+        -5.675, 0, 2.2,     // Vertex 0
+        -5.675, 0, 2.75,    // Vertex 1
+        -5.125, 0, 2.2,      // Vertex 2
+        -5.125, 0, 2.75,     // Vertex 3
+        -5.125, 3.3, 2.2,    // Vertex 4
+        -5.675, 3.3, 2.2,   // Vertex 5
 
         -0.275, 0, 2.2,    // Vertex 6
         -0.275, 0, 2.75,   // Vertex 7
@@ -242,29 +284,41 @@ function addCollumns(obj, x, y, z){
         0.275, 3.3, 2.2,   // Vertex 10
         -0.275, 3.3, 2.2,  // Vertex 11
 
-        4.85, 0, 2.2,       // Vertex 12
-        4.85, 0, 2.75,      // Vertex 13
-        5.4, 0, 2.2,      // Vertex 15
-        5.4, 0, 2.75,     // Vertex 16
-        5.4, 3.3, 2.2,    // Vertex 17
-        4.85, 3.3, 2.2      // Vertex 14
+        5.125, 0, 2.2,       // Vertex 12
+        5.125, 0, 2.75,      // Vertex 13
+        5.675, 0, 2.2,      // Vertex 14
+        5.675, 0, 2.75,     // Vertex 15
+        5.675, 3.3, 2.2,    // Vertex 16
+        5.125, 3.3, 2.2      // Vertex 17
     ];
 
     var indices = [
-        0, 1, 3,    // Triangle 1
-        0, 3, 2,    // Triangle 2
-        0, 2, 4,    // Triangle 3
-        0, 4, 5,    // Triangle 4
+        1, 3, 5,
+        5, 3, 4,
+        3, 2, 4,
+        1, 3, 0,
+        0, 3, 2,
+        0, 2, 5,
+        5, 2, 4,
+        1, 0, 5,
 
-        6, 7, 9,    // Triangle 5
+        7, 9, 6,
         6, 9, 8,
-        6, 8, 10,
-        6, 10, 11,
+        7, 9, 11,
+        11, 9, 10,
+        9, 8, 10,
+        7, 6, 11,
+        6, 8, 11,
+        11, 8, 10,
 
-        12, 13, 15,
+        13, 15, 17,
+        17, 15, 16,
+        13, 15, 12,
         12, 15, 14,
-        12, 14, 16,
-        12, 16, 17
+        15, 14, 16,
+        13, 12, 17,
+        12, 14, 17,
+        17, 14, 16
     ];
 
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
@@ -275,7 +329,7 @@ function addCollumns(obj, x, y, z){
     geometry.computeVertexNormals(); 
 
     var collumnsMaterial = new THREE.MeshBasicMaterial();
-    collumnsMaterial.color.set("royalblue");
+    collumnsMaterial.color.set("white");
 
     var collumnsMesh = new THREE.Mesh(geometry, collumnsMaterial);
     collumnsMesh.position.set(x, y, z);
@@ -292,32 +346,31 @@ function addLine(obj, x, y, z){
     geometry = new THREE.BufferGeometry();
 
     var vertices = [
-        -4.85, 0, 2.2,      // Vertex 0: bottom left
-        -2.475, 0, 2.2,     // Vertex 1: bottom right
-        -4.85, 0.2, 2.2,    // Vertex 2: top left
-        -2.475, 0.2, 2.2,    // Vertex 3: top right
+        -5.125, 0, 2.2,        // Vertex 0: bottom left
+        -2.475, 0, 2.2,       // Vertex 1: bottom right
+        -5.125, 0.2, 2.2,      // Vertex 2: top left
+        -2.475, 0.2, 2.2,     // Vertex 3: top right
 
-        -1.375, 0, 2.2,     // Vertex 4
-        -0.275, 0, 2.2,     // Vertex 5
-        -1.375, 0.2, 2.2,   // Vertex 6
-        -0.275, 0.2, 2.2,    // Vertex 7
+        -1.375, 0, 2.2,       // Vertex 4
+        -0.275, 0, 2.2,       // Vertex 5
+        -1.375, 0.2, 2.2,     // Vertex 6
+        -0.275, 0.2, 2.2,     // Vertex 7
 
-        0.275, 0, 2.2,
-        4.85, 0, 2.2,
-        0.275, 0.2, 2.2,
-        4.85, 0.2, 2.2
+        0.275, 0, 2.2,        // Vertex 8
+        5.125, 0, 2.2,         // Vertex 9
+        0.275, 0.2, 2.2,      // Vertex 10
+        5.125, 0.2, 2.2        // Vertex 11
     ];
 
     var indices = [
-        0, 1, 2,  // Triangle 1
-        2, 1, 3,  // Triangle 2
+        0, 1, 2,
+        2, 1, 3,
 
-        4, 5, 6,
+        4, 5, 6,  
         6, 5, 7,
 
         8, 9, 10,
         10, 9, 11
-
     ];
 
     var positionAttribute = new THREE.Float32BufferAttribute(vertices, 3);
